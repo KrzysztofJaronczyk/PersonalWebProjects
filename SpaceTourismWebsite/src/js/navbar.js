0503c8818e832f9984e3c9f66bad9527c214339f
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-03-21 18:30:45 +0100

    Add navbar-sidebar

--- SpaceTourismWebsite/src/js/navbar.js
+++ SpaceTourismWebsite/src/js/navbar.js
@@ -1,10 +1,16 @@
-const navItems = document.querySelectorAll('.nav-item');
+const navItems = document.querySelectorAll('.nav-item')
+const hamburgerMenu = document.querySelector('.hamburger')
+const navBar = document.querySelector('.nav-list')
 
-navItems.forEach((item) => {
-    item.addEventListener('click', () => {
-        navItems.forEach((item) => {
-        item.classList.remove('nav-item--active');
-        });
-        item.classList.add('nav-item--active');
-    });
-    });
+navItems.forEach(item => {
+	item.addEventListener('click', () => {
+		navItems.forEach(item => {
+			item.classList.remove('nav-item--active')
+		})
+		item.classList.add('nav-item--active')
+	})
+})
+
+hamburgerMenu.addEventListener('click', () => {
+	navBar.classList.toggle('nav-list--active')
+})
