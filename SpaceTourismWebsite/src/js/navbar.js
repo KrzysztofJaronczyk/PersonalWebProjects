e7925a1029d814cd1be677b050994fe776421bb2
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-03-22 21:08:05 +0100

    Finish the landing page

--- SpaceTourismWebsite/src/js/navbar.js
+++ SpaceTourismWebsite/src/js/navbar.js
@@ -6,11 +6,31 @@ navItems.forEach(item => {
 	item.addEventListener('click', () => {
 		navItems.forEach(item => {
 			item.classList.remove('nav-item--active')
+			toggleHamburger(false)
 		})
 		item.classList.add('nav-item--active')
+		navBar.classList.remove('nav-list--active')
 	})
 })
 
-hamburgerMenu.addEventListener('click', () => {
+hamburgerMenu.addEventListener('click', toggleHamburger)
+
+function toggleHamburger(bool = true) {
+	if (bool === false) {
+		navBar.classList.remove('nav-list--active')
+		hamburgerMenu.classList.remove('is-active')
+		return
+	}
 	navBar.classList.toggle('nav-list--active')
-})
+	hamburgerMenu.classList.toggle('is-active')
+}
+
+//click outside of the nav bar to close it
+function closeNav(e) {
+	if (!e.target.closest('.nav-list') && !e.target.closest('.hamburger')) {
+		navBar.classList.remove('nav-list--active')
+		hamburgerMenu.classList.remove('is-active')
+	}
+}
+
+document.addEventListener('click', closeNav)
